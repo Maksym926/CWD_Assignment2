846f9c88226cfddc472e055478d544aef28ae4ab
Maksym926 <maxchechotkin@gmail.com>
2026-04-13 14:49:18 +0100

    install the bootstrap and create Navbar component

---
+++ app/components/inventory/Navbar.js
@@ -0,0 +1,41 @@
+"use client";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Link from "next/link";
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+            <div className="container">
+                <Link className="navbar-brand" href="/inventory">
+                    Appliance Inventory
+                </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/inventory">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/inventory/add">Add</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/inventory/search">Search</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/inventory/update">Update</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/inventory/delete">Delete</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
+import Navbar from "./components/inventory/Navbar";
 
 export default function Home() {
     return <div>
-            <button>Add appliance</button>
-            <button>Edit appliance</button>
-            <button>Search appliance</button>
-            <button>Delete </button>
+            <Navbar />
            </div>;
 }
